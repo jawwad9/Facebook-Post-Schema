cac84eb211cca9ead4a2c47ecfa49a28dd508349
jawwad9 <jawwadmuhammad514@gmail.com>
2024-12-22 21:58:39 +0500

    complete

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,7 +4,8 @@ import { createPost } from '../controllers/post.controllers.js'
 import { userLike } from '../controllers/like.controllers.js'
 import { userComment } from '../controllers/comment.controllers.js'
 const router = express.Router()
-
+  
+ // routing 
 router.post('/post', createPost)
 router.post('/like', userLike)
 router.post('/comment', userComment)
